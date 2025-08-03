a49bba1f478e564fb4686514741376297491596c
aveA <aveA@AVE>
2025-08-03 12:18:08 +0300

    +

---
+++ Weather/Weather/Weather/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Weather.Pages;
+using Xamarin.Forms;
+
+namespace Weather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
